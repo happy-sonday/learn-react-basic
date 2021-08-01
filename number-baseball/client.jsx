fa975e81797e1c5dd489910a5adf53160ba66e42
happy-sonday <mrsmiler1106@gmail.com>
2021-08-01 23:50:27 +0900

    shouldComponentUpdate 예제

--- number-baseball/client.jsx
+++ number-baseball/client.jsx
@@ -1,6 +1,9 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
-import NumberBaseball from "./NumberBaseball";
 
-ReactDom.render(<NumberBaseball />, document.querySelector('#root'));
+import NumberBaseball from "./NumberBaseball";
+import RenderTest from "./RenderTest";
+
+//ReactDom.render(<NumberBaseball />, document.querySelector('#root'));
+ReactDom.render(<RenderTest />, document.querySelector('#root'));
