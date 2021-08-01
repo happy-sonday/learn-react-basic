6129cd333db413a83478204996f6f47701fc4bd8
happy-sonday <mrsmiler1106@gmail.com>
2021-08-01 22:24:58 +0900

    hooks 변경 에러 점검

--- number-baseball/NumberBaseball.jsx
+++ number-baseball/NumberBaseball.jsx
@@ -14,90 +14,89 @@ function getRandomNumber(){
 }
 
 
+const NumberBaseball = () =>{
 
-const NumberBaseball = () => {
+    const [result, setResult]= useState('');
+    const [value, setValue]= useState('');
+    const [answer, setAnswer]= useState(getRandomNumber);
+    const [tries, setTries]= useState([]);
 
-    const [result, setResult] = useState('');
-    const [value, setVale] = useState('');
-    const [answer, setAnswer] = useState(getRandomNumber());
-    const [tries, setTries] = useState([]);
 
-    const onSubmitForm=(e)=>{     
-
-        const {value, tries, answer} =this.state;
-        if(value === answer.join('')){
-        e.preventDefault();
-        setResult('홈런!');
-        setTries((prevTries)=>{
-            return[prevTries, {try : value, result:'홈런!'}]
-        });
-        setVale('');
+    const onSubmitForm = (e) => {
+    
+    console.log(answer);
+      e.preventDefault();
+      if (value === answer.join('')) {
+      setResult('홈런!');
+      setTries((prevTries)=>{
+          return[...prevTries, { try: value, result: '홈런!' }]
+      });
+      
+        
+        alert('게임을 다시 시작합니다!');
+        setValue('');
         setAnswer(getRandomNumber());
         setTries([]);
-    
         
-        }else{
-            const answerArray = value.split('').map((v)=>parseInt(v));
-            console.log('사용자 입력값',answerArray);
-            let strike = 0;
-            let ball = 0;
-            if( tries.length >=9){
-                
-                setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다.`);                
-                alert('게임을 다시 시작합니다');
-                setVale('');
-                setAnswer(getRandomNumber());
-                setTries([]);
-                    
-                                
-            }else{
-                for(let i = 0; i<4; i+=1){
-                    if(answerArray[i]===answer[i]){
-                        strike+=1;
-                    }else if(answer.includes(answerArray[i])){
-                        ball+=1;
-                    }
-                }
-
-                setTries((prevTries)=>{
-                    [...prevTries, { try: value, result:`${strike} 스트라이크, ${ball}볼 입니다`}]
-                });
-                setVale('');
-           
+        
+      } else { // 답 틀렸으면
+        const answerArray = value.split('').map((v) => parseInt(v));
+        let strike = 0;
+        let ball = 0;
+        if (tries.length >= 9) { // 10번 이상 틀렸을 때
+        
+        setResult(
+            `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`
+          );
+          alert('게임을 다시 시작합니다!');          
+            setValue('');
+            setAnswer(getRandomNumber());
+            setTries([]);
+          
+        } else {
+          for (let i = 0; i < 4; i += 1) {
+            if (answerArray[i] === answer[i]) {
+              strike += 1;
+            } else if (answer.includes(answerArray[i])) {
+              ball += 1;
             }
-        };
-    }
-
-    const onChangeInput=(e)=>{        
-
-        setVale(e.target.value);
+          }
+          setTries((prevTries)=>{
+            return [...prevTries, { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다`}]             
+            
+          });
+          setValue('');
+        }
+      }
     };
-
-
-
-
-        return (
-            <>
-                <h1>{result}</h1>
-                <form onSubmit={onSubmitForm}>
-                    <input maxLength={4} value={value} onChange={onChangeInput} />
-                </form>
-                <div>시도: {tries.length}</div>
-                <ul>
-                    
-                    {tries.map((item, idx)=>{
-                        return(                            
-                          <Try key={`${idx+1}차 시도:`}  tryInfo={item} />                 
-                            
-                        );
-                    })}
-                </ul>
-            </>
-        )
-   
-
+  
+    const onChangeInput = (e) => {
+      setValue(e.target.value)
+      
+    };
+  
+  
+  
+  
+  
+      return (
+        <>
+          <h1>{result}</h1>
+          <form onSubmit={onSubmitForm}>
+            <input maxLength={4} value={value} onChange={onChangeInput} />
+          </form>
+          <div>시도: {tries.length}</div>
+          <ul>
+            {tries.map((v, i) => {
+              return (
+                <Try key={`${i + 1}차 시도 :`} tryInfo={v} />
+              );
+            })}
+          </ul>
+        </>
+      );
     
-}
+  }
 
 
 export default NumberBaseball; //import NumberBaseball;
--- number-baseball/Try.jsx
+++ number-baseball/Try.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Try = ({tryInfo})=>{
     return(
         <li>
-        <div className="">{tryInfo.try}</div>
-        <div className="">{tryInfo.result}</div>
-   </li>
+            <div className="">{tryInfo.try}</div>
+            <div className="">{tryInfo.result}</div>
+        </li>
     )
 }
 
