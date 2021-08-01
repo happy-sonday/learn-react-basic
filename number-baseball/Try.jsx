d7bf37d6f3881b599da16447dac478e326f0bf32
happy-sonday <mrsmiler1106@gmail.com>
2021-08-01 15:42:49 +0900

    숫자야구

--- number-baseball/Try.jsx
+++ number-baseball/Try.jsx
@@ -4,13 +4,8 @@ class Try extends Component{
     render(){
         return(
             <li>
-                    <b>{this.props.value.fruit}- {this.props.index}</b>
-                    <br/>
-                    맛은 {this.props.value.taste}
-                    <div>컨텐츠</div>
-                    <div>컨텐츠1</div>
-                    <div>컨텐츠2</div>
-                    <div>컨텐츠3</div>
+                 <div className="">{this.props.tryInfo.try}</div>
+                 <div className="">{this.props.tryInfo.result}</div>
             </li>
         )
     }
