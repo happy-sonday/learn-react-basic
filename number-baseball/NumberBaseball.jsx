e6993a23da24817aefa7f41e64a8a49d57d1f1fc
happy-sonday <mrsmiler1106@gmail.com>
2021-08-01 14:41:35 +0900

    바인딩 및 화살표 함수를 쓰지 않는 경우 초기화 문법

--- number-baseball/NumberBaseball.jsx
+++ number-baseball/NumberBaseball.jsx
@@ -7,23 +7,48 @@ function getRandomNumber(){
 
 
 class NumberBaseball extends Component{
-    state ={
-        result:'',
-        value:'',
-        answer:getRandomNumber(),
-        tries:[],
 
-    };
+    constructor(props){
+        super(props);
+        this.state={
+            result:'',
+            value:'',
+        }
+
+        this.onSubmitForm = this.onSubmitForm.bind(this);
+        this.onChangeInput= this.onChangeInput.bind(this);
+    }
+
+    
+    // state ={
+    //     result:'',
+    //     value:'',
+    //     answer:getRandomNumber(),
+    //     tries:[],
+
+    // };
 
     fruits=[{fruit: '사과',taste:'맛있다'},{fruit: '바나나',taste:'맛있다'},{fruit: '포도',taste:'맛있다'},{fruit: '귤',taste:'상큼하다'},{fruit: '배',taste:'시원'},{fruit: '밤',taste:'달다'},{fruit: '사과',taste:'달콤하다'}];
 
-    onSubmitForm=()=>{
+    onSubmitForm(e){
+        e.preventDefault();
+        console.log(this.state.value);
         
     };
 
-    onChangeInput=()=>{
-        
+    onChangeInput(e){
+        console.log(this)
+        this.setState({
+            value : e.target.value,
+        })
     };
+    // onSubmitForm=()=>{
+    
+    // };
+
+    // onChangeInput=()=>{
+        
+    // };
 
     render(){
         return (
@@ -32,12 +57,12 @@ class NumberBaseball extends Component{
                 <form onSubmit={this.onSubmitForm}>
                     <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
                 </form>
-                <div>시도: {this.state.tries.length}</div>
+              {/*   <div>시도: {this.state.tries.length}</div> */}
                 <ul>
                     <h3>객체배열</h3>
                     {this.fruits.map((item, idx)=>{
                         return(                            
-                          <Try value={item} index={idx} />                 
+                          <Try key={item.fruit+item.taste}  value={item} index={idx} />                 
                             
                         );
                     })}
