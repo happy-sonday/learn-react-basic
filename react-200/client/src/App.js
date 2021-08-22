67c7dc0a64d3c8282d2490cc0aa86903d471cc78
happy-sonday <mrsmiler1106@gmail.com>
2021-08-22 16:10:49 +0900

    redux basic

--- react-200/client/src/App.js
+++ react-200/client/src/App.js
@@ -1,71 +1,24 @@
 /* import "./App.css"; */
 
 import "bootstrap/dist/css/bootstrap.css";
-import GrandParent from "./ContextApi/GrandParent";
+import React, { Component } from "react";
 
-import KeyboardEvnt from "./eventHandle/KeyboardEvnt";
-import MouseMoveEvnt from "./eventHandle/MouseEvnt";
-import RefBasic from "./eventHandle/RefBasic";
-import SubmitEvnt from "./eventHandle/SubmitEvnt";
-import CurringBasic from "./funcComponent/CurringBasic";
-import CurringBasic2 from "./funcComponent/CurringBasic2";
-import HOCBasic from "./funcComponent/HOCBasic";
+import StrAddButton from "./StrAddButton";
 
-function App() {
-  return (
-    <div>
-      <h1>Start React 200!</h1>
-      <p>CSS 적용하기</p>
-      {/*       <PropsObj objectJSON={{ react: "리액트", twohundred: "200" }} /> */}
-      {/* <PropsObj
-        objectJSON={JSON.stringify({ react: "리액트", twohundred: 200 })}
-      /> */}
-      {/* <ReqProps reactNum={200}></ReqProps> */}
-      {/*  <DefaultVal /> */}
-      {/* <NodeChildren>
-        <span>node from App.js</span>
-        <ul>
-          <li className="item">아이템1</li>
-          <li className="item">아이템2</li>
-          <li className="item">아이템3</li>
-        </ul>
-      </NodeChildren> */}
-      {/* <BasicState reactString={"state-learn-react"}></BasicState> */}
-      {/* <R25_SetState /> */}
-      {/*  <ClassComponent></ClassComponent> */}
-      {/* <R28_PureComponent /> */}
-      {/* <R029_ShallowEqual /> */}
-      {/* <R30_FunctionComponent contents="THIS IS FUNCTION COMPONENT" /> */}
-      {/* <R31Hook /> */}
-      {/*  <R032_Fragments /> */}
-      {/* <ReturnMAp /> */}
-      {/* <ReactstrapAlerts /> */}
-      {/* <StrapBadge /> */}
-      {/* <R056SweetAlert2Basic /> */}
-      {/*  <FetchGet /> */}
-      {/* <AxiousPost /> */}
-      {/* <AxiousGet /> */}
-      {/* <CallBackBasic /> */}
-      {/* <PromiseBasic /> */}
-      {/* <Reject /> */}
-      {/* NOTE:part3 */}
-      {/* <ClickEvnt /> */}
-      {/* <ChangEvnt /> */}
-      <MouseMoveEvnt />
-      <KeyboardEvnt />
-      <SubmitEvnt />
-      <h1>Ref Exam</h1>
-      <RefBasic />
-      <CurringBasic />
-      <CurringBasic2 />
-      <HOCBasic name="App to React200" />
-      <br />
-      <br />
-      Context API Basic
-      {/* <GrandParent /> */}
-      <GrandParent />
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log(this.props.store.getState().data);
+    return (
+      <div>
+        <h1>Start React 200!</h1>
+        <span>{this.props.store.getState().data.str}</span>
+        <br />
+        <StrAddButton store={this.props.store} />
+        <br />
+        버튼을 누르면 200 문자열 추가
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-200/client/src/index.js
+++ react-200/client/src/index.js
@@ -2,15 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <App />,
+import { createStore } from "redux";
+import reducers from "./reducers";
+const store = createStore(reducers);
 
-  document.getElementById("root")
-);
+const listener = () => {
+  ReactDOM.render(
+    <App store={store} />,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    document.getElementById("root")
+  );
+};
+
+store.subscribe(listener);
+listener();
+// ReactDOM.render(
+//   <App />,
+
+//   document.getElementById("root")
+// );
